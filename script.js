6511cd6d7e7245b32d990af7ed92b1b719445f1b
cod3ngg <150222307+cod3ngg@users.noreply.github.com>
2024-09-11 21:23:57 +0800

    Added UI to RPS game and new mechanics

--- script.js
+++ script.js
@@ -1,57 +1,91 @@
 let humanScore = 0;
 let computerScore = 0;
 let round = 1;
-let repeat;
+
+const container = document.getElementsByClassName("container");
+const mainTitle = document.querySelector("#main-title");
+const battleground = document.querySelector("#battleground");
+const getButton = document.querySelectorAll("button");
+const humanOut = document.querySelector("#human");
+const botOut = document.querySelector("#computer");
+const humanOutScore = document.querySelector("#humanscore");
+const botOutScore = document.querySelector("#botscore");
+const footer = document.querySelector("#footer")
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice > computerChoice) {
+    if (humanChoice == 0 && computerChoice == 2) {
         humanScore++;
-        alert(`HUMAN WINS!\n${humanScore} - ${computerScore}`);
+        footer.textContent = "Human Won!";
+        console.log("Human Won!");
+    }
+    else if (humanChoice == 2 && computerChoice == 0) {
+        computerScore++;
+        footer.textContent = "Computer Won!";
+        console.log("Computer Won!");
     }
     else if(humanChoice == computerChoice) {
-        alert(`DRAW!\n${humanScore} - ${computerScore}`);
-
+        footer.textContent = "Draw!";
+        console.log("Draw");
+    }
+    else if (humanChoice > computerChoice) {
+        humanScore++;
+        footer.textContent = "Human Won!";
+        console.log("Human Won!");
     }
     else {
         computerScore++;
-        alert(`COMPUTER WINS!\n${humanScore} - ${computerScore}`);
+        footer.textContent = "Computer Won!";
+        console.log("Computer Won!");        
     }
-}
+    humanOutScore.textContent = `${humanScore}`;
+    botOutScore.textContent = `${computerScore}`;
+    round++;
 
-
-function getHumanChoice() {
-    let humanAns = window.prompt(`Round ${round} - 5\nChoose your weapon:\n1 - ROCK\n2 - PAPER\n3 - SCISSOR`);
-    return humanAns;
+    if (humanScore == 5 || computerScore == 5) {
+        overallWin();
+    }
 }
 
 function getComputerChoice(maxInt) {
     return Math.floor(Math.random() * maxInt);
 }
 
-function overallWin() {
-    if (humanScore > computerScore) {
-        alert(`HUMAN IS DA WINNER`);
-    } else if (humanScore == computerScore) {
-        alert(`NO-ONE IS DA WINNER`);
-    } else {
-        alert(`COMPUTER IS DA WINNER`);
+function displayChoice(selectChoice) {
+    switch (selectChoice) {
+        case 0:
+            return "✊";
+        case 1:
+            return "✋";
+        case 2:
+            return "✌";
+        default:
+            return "💀";
     }
 }
 
-function playGame() {
-    for (let i = 1; i < 6; i++) {
-        const humanSelect = getHumanChoice();
-        const computerSelect = getComputerChoice(3);
-        playRound(humanSelect, computerSelect);
-        round++;
+function overallWin() {
+    if (humanScore > computerScore) {
+        mainTitle.textContent = `WINNER: HUMAN`;
+    } else {
+        mainTitle.textContent = `WINNER: COMPUTER`;
     }
-    overallWin();
     round = 1;
     humanScore = 0;
     computerScore = 0;
 }
 
-do {
-    playGame();
-    repeat = confirm(`Wanna play another game?`);
-} while (repeat);
+function playGame(human) {
+    const humanSelect = parseInt(human);
+    humanOut.textContent = displayChoice(humanSelect);
+    const computerSelect = getComputerChoice(3);
+    console.log(computerSelect);
+    botOut.textContent = displayChoice(computerSelect);
+    mainTitle.textContent = `Round #${round}`;
+    playRound(humanSelect, computerSelect);       
+}
+
+getButton.forEach((button) => {
+button.addEventListener("click", () => {
+    playGame(button.value);
+    });
+});
